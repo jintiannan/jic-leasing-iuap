263c1bcf9ca9aa488763a8cd4483cce344104e8a
clay <591477472@qq.com>
2019-10-14 20:44:46 +0800

    客户模块、请求后台

---
+++ ucf-apps/customer/src/routes/CustomerCorpApply/components/FormView.jsx
@@ -0,0 +1,294 @@
+import React, {Component} from 'react';
+import {Button, Checkbox, Col, Collapse, Form, FormControl, Icon, Label, Radio, Row, Select, Switch} from 'tinper-bee';
+import {deepClone} from "utils";
+import {SelectField} from 'components/RowField/SelectField'
+import FormSplitHeader from 'components/FormSplitHeader'
+import DatePicker from "tinper-bee/lib/Datepicker";
+import TableFormRef from 'components/FormRef/TableFormRef';
+import FormInputNumber from '../../../../../../ucf-common/src/components/FormRef/FormInputNumber';
+import FormTreeRef from "../../../../../../ucf-common/src/components/FormTreeRef/FormTreeRef";
+
+import './index.less';
+import {enumConstant} from "../../../../../../ucf-common/src/utils/enums";
+
+const FormItem = Form.FormItem;
+
+class FormView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: true,
+            open1: true,
+            open2: true,
+            open3: true,
+            open4: true,
+            open5: true,
+            open6: true,
+            open7: true,
+            open8: true,
+
+        };
+    }
+
+    //组件生命周期方法-在渲染前调用,在客户端也在服务端
+    componentWillMount() {
+
+    }
+
+    //组件生命周期方法-在第一次渲染后调用，只在客户端
+    componentDidMount() {
+        this.props.onRef(this);
+    }
+
+    //组件生命周期方法-在组件接收到一个新的 prop (更新后)时被调用
+    componentWillReceiveProps(nextProps) {
+    }
+
+    handleChange = (value) => {
+        if (value == '暂存') {
+            this.props.form.setFieldsValue({'meetingnper': 555});
+        }
+    };
+    form = [
+        {label: '客户名称', field: 'customerName', com: FormControl, required: true},
+        {label: '客户编号', field: 'customerCode', com: FormControl, required: true, disabled: true},
+        {label: '客户英文名', field: 'customer_eng_name', com: FormControl, required: true},
+        {label: '客户简称', field: 'customer_short', com: FormControl, required: true},
+        {label: '是否授权征信客户', field: 'if_warrant_cust', com: Select, data: enumConstant("yesOrNo"), required: true},
+        {label: '客户性质', field: 'customer_property', com: FormTreeRef, required: true},
+        {label: '客户性质(内部)', field: 'customer_property_in', com: FormTreeRef, required: true},
+        {label: '客户类型', field: 'cusotmer_class_temp', com: Select, required: true},
+        {label: '经济性质', field: 'economic_type', com: TableFormRef, required: true},
+        {label: '注册地址', field: 'reg_address', com: FormControl, required: true, col: 12},
+    ];
+    form1 = [
+        // {label: '行业门类', field: 'industry', com: FormTreeRef},
+        // {label: '行业门类(大类)', field: 'industry1', com: FormTreeRef},
+        // {label: '行业门类(中类)', field: 'industry2', com: FormTreeRef},
+        // {label: '行业门类(小类)', field: 'industry3', com: FormTreeRef},
+        {label: '资产总额(万元)', field: 'assets_total', com: FormInputNumber},
+        {label: '营业收入(万元)', field: 'operating_income', com: FormInputNumber},
+        {label: '学校等级', field: 'school_grade', com: Select, data: enumConstant("yesOrNo")},
+        {label: '医院等级', field: 'hospital_grade', com: Select, data: enumConstant("yesOrNo")},
+        {label: '企业规模(人行)', field: 'enter_scale_pbc', com: Select, data: enumConstant("yesOrNo")},
+        {label: '企业规模(内部管理)', field: 'enter_scale_inner', com: Select, data: enumConstant("yesOrNo")},
+        {label: '企业规模(四部委)', field: 'enter_scale_6m', com: Select, data: enumConstant("yesOrNo")},
+    ];
+    form2 = [
+        {label: '注册登记号类型', field: 'reg_number_type', com: Select, data: enumConstant("yesOrNo")},
+        {label: '注册登记号', field: 'reg_number', com: FormControl},
+        {label: '证件类型', field: 'identity_type', com: Select, data: enumConstant("yesOrNo")},
+        {label: '证件有效期(开始)', field: 'start_date_identity', com: DatePicker},
+        {label: '证件有效期(截止)', field: 'end_date_identity', com: DatePicker},
+        {label: '最新年检日期', field: 'recent_inspect_date', com: DatePicker},
+        {label: '贷款卡号', field: 'loan_card_no', com: FormControl},
+        {label: '贷款卡最新年审时间', field: 'recent_inspect_date_lc', com: DatePicker},
+        {label: '贷款卡是否有效', field: 'if_valid_lc', com: Select, data: enumConstant("yesOrNo")},
+        {label: '贷款卡最新年审结果', field: 'recent_inspect_result_lc', com: FormControl},
+        {label: '机构信用代码', field: 'org_credit_code', com: FormControl},
+        {label: '营业执照号码', field: 'license_no', com: FormControl},
+        {label: '成立日期', field: 'establish_date', com: DatePicker},
+        {label: '营业执照登记日', field: 'start_date_license', com: DatePicker},
+        {label: '营业执照到期日', field: 'end_date_license', com: DatePicker},
+        {label: '营业执照最新年审日', field: 'recent_inspect_date_license', com: DatePicker},
+        {label: '法定代表人(文本类型)', field: 'legal_representative', com: FormControl},
+        // {label: '法定代表人', field: 'pk_customer_person', com: TableFormRef},
+        // {label: '证件类型', field: 'pk_customer_person.identity_type', com: Select, data: enumConstant("yesOrNo")},
+        // {label: '证件号码', field: 'industpk_customer_person.identity_nory', com: FormControl},
+        {label: '实际控制人(文本类型)', field: 'actual_controller', com: FormControl},
+        // {label: '实际控制人', field: 'actual_control', com: TableFormRef},
+        // {label: '证件类型', field: 'actual_control.identity_type', com: Select, data: enumConstant("yesOrNo")},
+        // {label: '证件号码', field: 'actual_control.identity_no', com: FormControl},
+        {label: '注册资本币种', field: 'capital_cur', com: TableFormRef},
+        {label: '注册资本', field: 'capital', com: FormInputNumber},
+        {label: '实收资本币种', field: 'capital_cur_paidin', com: TableFormRef},
+        {label: '实收资本', field: 'capital_paidin', com: FormInputNumber},
+        {label: '税务登记证号(国税)', field: 'national_tax', com: FormControl},
+        {label: '税务登记证号(地税)', field: 'land_tax', com: FormControl},
+        {label: '经营范围(限200个汉字)', field: 'bussiness_scope', com: FormControl, col: 12, class: 'textarea'},
+        {label: '主营业务(限200个汉字)', field: 'primary_bussiness', com: FormControl, col: 12, class: 'textarea'},
+    ];
+
+    form3 = [
+        {label: '是否集团公司(母公司)', field: 'if_group_company', com: Select, data: enumConstant("yesOrNo")},
+        {label: '有无进出口经营', field: 'if_import_export', com: Select, data: enumConstant("yesOrNo")},
+        {label: '是否上市公司', field: 'if_listed_company', com: Select, data: enumConstant("yesOrNo")},
+        {label: '是否政府融资背景', field: 'if_gov_platform', com: Select, data: enumConstant("yesOrNo")},
+        {label: '是否科技型企业', field: 'if_technical_corp', com: Select, data: enumConstant("yesOrNo")},
+        {label: '科技型企业类型', field: 'technical_type', com: Select, data: enumConstant("yesOrNo")},
+        {label: '是否环保行业企业', field: 'if_environmental_corp', com: Select, data: enumConstant("yesOrNo")},
+        {label: '是否重点监测客户', field: 'if_important_corp', com: Select, data: enumConstant("yesOrNo")},
+    ];
+    form4 = [
+        // {label: '所在国家(地区)', field: 'country', com: Select, data: enumConstant("yesOrNo")},
+        // {label: '区域', field: 'region', com: Select, data: enumConstant("yesOrNo")},
+        // {label: '注册地隶属', field: 'reg_address_membership', com: Select, data: enumConstant("yesOrNo")},
+        // {label: '地区(省)', field: 'province', com: Select, data: enumConstant("yesOrNo")},
+        // {label: '地区(市)', field: 'city', com: Select, data: enumConstant("yesOrNo")},
+        // {label: '地区(区/县)', field: 'district', com: Select, data: enumConstant("yesOrNo")},
+        {label: '实际告知地址', field: 'inform_address', com: FormControl, col: 12},
+        {label: '实际告知地址邮编', field: 'inform_address_zip', com: FormControl},
+        {label: '通讯地址', field: 'comm_address', com: FormControl, col: 12},
+        {label: '通讯地址邮编', field: 'comm_address_zip', com: FormControl},
+        {label: '实际办公地址', field: 'office_address', com: FormControl, col: 12},
+        {label: '实际办公地址邮编', field: 'office_address_zip', com: FormControl},
+        {label: '实际办公地所有权', field: 'office_ownership', com: FormControl},
+        {label: '备注', field: 'remarks', com: FormControl, col: 12, class: 'textarea'},
+    ];
+    form5 = [
+        {label: '客户洗钱风险评级', field: 'enter_rating', com: Select, data: enumConstant("yesOrNo")},
+    ];
+    form6 = [
+        {label: '公司网址', field: 'company_website', com: FormControl},
+        {label: '传真', field: 'fax', com: FormControl},
+        {label: '公司邮箱', field: 'company_mailbox', com: FormControl},
+        {label: '电话', field: 'phone', com: FormControl},
+        {label: '开票说明', field: 'invoice_explain', com: Select, data: enumConstant("yesOrNo")},
+    ];
+    form7 = [
+        {label: '部门名称', field: 'pk_dept', com: TableFormRef, disabled: true},
+        {label: '客户经理', field: 'pk_prj_manager', com: TableFormRef, disabled: true},
+        {label: '操作人', field: 'pk_operator', com: TableFormRef, disabled: true},
+        {label: '操作日期', field: 'pk_operator', com: DatePicker, disabled: true},
+        {label: '操作时间', field: 'pk_operator', com: DatePicker, disabled: true},
+        {label: '最新变更人', field: 'pk_operator', com: TableFormRef, disabled: true},
+        {label: '最新变更日期', field: 'pk_operator', com: DatePicker, disabled: true},
+        {label: '最新变更时间', field: 'pk_operator', com: DatePicker, disabled: true},
+        {label: '审核人', field: 'pk_operator', com: TableFormRef, disabled: true},
+        {label: '审核日期', field: 'pk_operator', com: DatePicker, disabled: true},
+        {label: '审核时间', field: 'pk_operator', com: DatePicker, disabled: true},
+        {label: '机构', field: 'pk_dept', com: TableFormRef, disabled: true},
+        {label: '二级业务领域', field: 'pk_dept', com: TableFormRef, disabled: true},
+    ];
+
+    render() {
+        const {getFieldProps, getFieldError} = this.props.form;
+        // let _formObject = deepClone(this.props.formObject);
+        let _formObject = this.props.formObject;
+        let formObject = deepClone(_formObject);
+        const loop = data => data.map((value, key) => {
+            return (
+                <Col md={value.col ? value.col : 4} xs={value.col ? value.col : 4} sm={value.col ? value.col : 4}>
+                    <FormItem
+                        className={(value.col === 12 ? (value.class && value.class === 'textarea' ? "remark flex jic-textArea" : "remark flex ") : '')}>
+                        <Label className={value.col === 12 ? "line-height-32" : ''}>
+                            {(value.required && value.required === true) ?
+                                <Icon type="uf-mi" className='mast'></Icon> : ''}
+                            {value.label}
+                        </Label>
+                        <value.com {...this.props}
+                                   title={value.label}
+                                   name={value.field}
+                                   disabled={value.disabled ? value.disabled : !this.props.isEdit}
+                                   data={value.data ? value.data : ''}
+                                   toThousands={value.com === FormInputNumber ? (value.toThousands ? value.toThousands : true) : ''}  //是否显示千分位
+                                   precision={value.com === FormInputNumber ? (value.precision ? value.precision : 2) : ''} //保留2位小数
+                                   componentClass={value.class ? value.class : 'input'}
+                                   {
+                                       ...getFieldProps(value.field, {
+                                           initialValue: formObject[value.field],
+                                           rules: [{
+                                               required: true,
+                                           }],
+                                       })
+                                   }>
+                        </value.com>
+                    </FormItem>
+                </Col>)
+        });
+
+        return (
+            <div className="jic-form">
+                <div>
+                    <div  className={this.form.length === 0 ? "display-none": ""}> <span onClick={() => this.setState({open: !this.state.open})}>
+                                        <FormSplitHeader open={this.state.open} title={'客户基本信息'}/>
+                                      </span>
+                    </div>
+                    <Collapse in={this.state.open}>
+                        <Form>
+                            {loop(this.form)}
+                        </Form>
+                    </Collapse>
+
+                    <div  className={this.form1.length === 0 ? "display-none": ""}>
+                                        <span onClick={() => this.setState({open1: !this.state.open1})}>
+                                            <FormSplitHeader open={this.state.open1} title={'客户规模信息'}/>
+                                        </span>
+                    </div>
+                    <Collapse in={this.state.open1}>
+                        <Form>
+                            {loop(this.form1)}
+                        </Form>
+                    </Collapse>
+                    <div  className={this.form2.length === 0 ? "display-none": ""}>
+                    <span onClick={() => this.setState({open2: !this.state.open2})}>
+                        <FormSplitHeader open={this.state.open2} title={'客户证件信息'}/>
+                    </span>
+                    </div>
+                    <Collapse in={this.state.open2}>
+                        <Form>
+                            {loop(this.form2)}
+                        </Form>
+                    </Collapse>
+                    <div  className={this.form3.length === 0 ? "display-none": ""}>
+                    <span onClick={() => this.setState({open3: !this.state.open3})}>
+                        <FormSplitHeader open={this.state.open3} title={'客户重要标志'}/>
+                    </span>
+                    </div>
+                    <Collapse in={this.state.open3}>
+                        <Form>
+                            {loop(this.form3)}
+                        </Form>
+                    </Collapse>
+
+                    <div className={this.form4.length === 0 ? "display-none": ""}>
+                    <span onClick={() => this.setState({open4: !this.state.open4})}>
+                        <FormSplitHeader open={this.state.open4} title={'客户地址信息'}/>
+                    </span>
+                    </div>
+                    <Collapse in={this.state.open4}>
+                        <Form>
+                            {loop(this.form4)}
+                        </Form>
+                    </Collapse>
+
+                    <div className={this.form5.length === 0 ? "display-none": ""}>
+                    <span onClick={() => this.setState({open5: !this.state.open5})}>
+                        <FormSplitHeader open={this.state.open5} title={'客户评级信息'}/>
+                    </span>
+                    </div>
+                    <Collapse in={this.state.open5}>
+                        <Form>
+                            {loop(this.form5)}
+
+                        </Form>
+                    </Collapse>
+
+                    <div className={this.form6.length === 0 ? "display-none": ""}>
+                    <span onClick={() => this.setState({open6: !this.state.open6})}>
+                        <FormSplitHeader open={this.state.open6} title={'客户其他信息'}/>
+                    </span>
+                    </div>
+                    <Collapse in={this.state.open6}>
+                        <Form>
+                            {loop(this.form6)}
+                        </Form>
+                    </Collapse>
+                    <div className={this.form7.length === 0 ? "display-none": ""}>
+                    <span onClick={() => this.setState({open7: !this.state.open7})}>
+                        <FormSplitHeader open={this.state.open7} title={'操作信息'}/>
+                    </span>
+                    </div>
+                    <Collapse in={this.state.open7}>
+                        <Form>
+                            {loop(this.form7)}
+                        </Form>
+                    </Collapse>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Form.createForm()(FormView);
---
+++ ucf-apps/customer/src/routes/CustomerCorpModify/components/ListView.jsx
@@ -0,0 +1,230 @@
+import React, {Component} from 'react';
+import {actions} from 'mirrorx';
+import Grid from 'components/Grid';
+import {deepClone, getHeight} from "utils";
+import {genGridColumn,checkListSelect} from "utils/service";
+
+import './index.less';
+
+class ListView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tableHeight: 0,
+            filterable: false,
+            record: {}, // 存储关联数据信息
+            isGrid:true,//是否列表界面 true:列表界面 false:卡片界面
+            formView:'none',
+            listView:'',
+            selectList:[],
+        }
+    }
+
+
+    //组件生命周期方法-在渲染前调用,在客户端也在服务端
+    componentWillMount() {
+        //计算表格滚动条高度
+        this.resetTableHeight(false);
+        this.gridColumn = [...genGridColumn(this.grid)];
+    }
+
+    //组件生命周期方法-在第一次渲染后调用，只在客户端
+    componentDidMount() {
+        actions.customerCorpModify.loadList(this.props.queryParam);
+    }
+
+    //组件生命周期方法-在组件接收到一个新的 prop (更新后)时被调用
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    /**
+     * 跳转到指定页数据
+     * @param {Number} pageIndex 跳转指定页数
+     */
+    freshData = (pageIndex) => {
+        let queryParam = deepClone(this.props.queryParam); // 深拷贝查询条件从 action 里
+        queryParam['pageIndex'] = pageIndex;
+        actions.customerCorpModify.loadList(queryParam);
+    };
+
+    /**
+     * 设置每页显示行数
+     * @param {Number} index 跳转指定页数
+     * @param {Number} value 设置一页数据条数
+     */
+    onDataNumSelect = (index, value) => {
+        let queryParam = deepClone(this.props.queryParam); // 深拷贝查询条件从 action 里
+        queryParam['pageSize'] = value;
+        queryParam['pageIndex'] = 0;
+        if (value && value.toString().toLowerCase() === "all") { // 对分页 pageSize 为 all 进行处理，前后端约定
+            pageSize = 1;
+        }
+        actions.customerCorpModify.loadList(queryParam);
+    };
+
+    /**
+     * 行单击事件,同步行首checkbox
+     * 可能会有性能问题,暂时实现功能,待后期再取舍
+     * #关闭功能,如果有页面特殊要求再打开#
+     */
+    onRowSelect = (record, index, event) => {
+        // console.log('行点击事件');
+        // let _record = deepClone(record);
+        // _record._checked = _record._checked ? false : true;
+        // let param = {
+        //     record:_record,
+        //     index:index,
+        // }
+        // let _selectedList = deepClone(this.props.selectedList);
+        // if(_record._checked){
+        //     _selectedList.push(_record);
+        // } else {
+        //     _selectedList.splice(_selectedList.findIndex(item => item.pk === record.pk), 1)
+        // }
+        // actions.projectInfo.updateRowData(param,index);
+        // actions.projectInfo.updateState({ selectedList : _selectedList });
+
+
+
+    };
+
+    /**
+     * 点击row选择框触发绑定数据对象
+     * 绑定选中数据数组到数据模型中
+     */
+    getSelectedDataFunc = (selectedList,record,index) => {
+        let { list } = this.props;
+        let _list = deepClone(list);
+        let _selectedList = deepClone(selectedList);
+        let _formObj = {};
+        if (index != undefined) {
+            _list[index]['_checked'] = !_list[index]['_checked'];
+        } else {
+            if(_selectedList && _selectedList.length > 0){
+                _list.map(item => {
+                    if (!item['_disabled']) {
+                        item['_checked'] = true;
+                    }
+                });
+            } else {
+                _list.map(item => {
+                    if (!item['_disabled']) {
+                        item['_checked'] = false;
+                    }
+                });
+            }
+        }
+        if(_selectedList && _selectedList.length == 1){
+            _formObj = deepClone(_selectedList[0]);
+        }
+
+        actions.customerCorpModify.updateState({ list : _list,selectedList : _selectedList,formObject : _formObj});
+    };
+    /**
+     * 重置表格高度计算回调
+     *
+     * @param {Boolean} isopen 是否展开
+     */
+    resetTableHeight = (isopen) => {
+        let tableHeight = 0;
+        tableHeight = getHeight() - 155;
+        this.setState({ tableHeight });
+    };
+
+    //列属性定义
+    grid = [
+        {title: '单据状态', key: 'billstatus', type: '0'},
+        {title: '客户编号', key: 'customerCode', type: '0'},
+        {title: '客户名称', key: 'customerName', type: '0',width: 240},
+        {title: '组织机构代码证', key: 'identity_no', type: '0'},
+        {title: '客户性质', key: 'customer_property.name', type: '0'},
+        {title: '客户性质(内部)', key: 'customer_property_in.name', type: '0'},
+        {title: '行业门类', key: 'industry.name', type: '0'},
+        {title: '行业门类(大类)', key: 'industry1.name', type: '0'},
+        {title: '行业门类(中类)', key: 'industry2.name', type: '0'},
+        {title: '地区(省)', key: 'province.name', type: '0'},
+        {title: '地区(市)', key: 'city.name', type: '0'},
+        {title: '部门名称', key: 'pk_dept', type: '0'},
+        {title: '客户号', key: 'customer_no', type: '0'},
+        {title: '传真', key: 'fax', type: '0'},
+        {title: '电话', key: 'phone', type: '0'},
+        {title: '实际办公地址', key: 'office_address', type: '0'},
+        {title: '实际办公地址邮编', key: 'office_address_zip', type: '0'},
+        {title: '客户类型', key: 'cusotmer_class_temp', type: '0'},
+        // {title: '业务领域', key: 'industry_type', type: '0'},
+        // {title: '二级业务领域', key: 'industry_type1', type: '0'},
+        // {title: '学校等级', key: 'school_grade', type: '0'},
+        // {title: '医院等级', key: 'hospital_grade', type: '0'},
+        // {title: '隶属', key: 'subjection', type: '0'},
+        {title: '称号', key: 'title', type: '0'},
+        {title: '从业人数', key: 'employee_num', type: '0'},
+        {title: '备注', key: 'remarks', type: '0'},
+        {title: '客户状态', key: 'customer_status', type: '0'},
+        // {title: '客户经理', key: 'pk_prj_manager', type: '0'},
+        // {title: '最新变更人', key: 'pk_operator_lst', type: '0'},
+        {title: '操作日期', key: 'operate_date', type: '0'},
+        {title: '最新变更日期', key: 'operate_date_lst', type: '0'},
+        {title: '版本号', key: 'version_num', type: '0'},
+        {title: '注册登记号类型', key: 'reg_number_type', type: '0'},
+        {title: '注册登记号', key: 'reg_number', type: '0'},
+        {title: '是否授权征信客户', key: 'if_warrant_cust', type: '0'},
+        {title: '机构信用代码', key: 'org_credit_code', type: '0'},
+        {title: '生效日期', key: 'effective_date', type: '0'},
+
+    ];
+    //列属性定义=>通过前端service工具类自动生成
+    gridColumn = [];
+
+    render() {
+        let { tableHeight} = this.state;
+        return (
+            <div className="grid-parent" style={{display:this.state.listView}}>
+                    <Grid
+                        ref={(el) => this.grid = el} //存模版
+                        columns={this.gridColumn} //字段定义
+                        data={this.props.list} //数据数组
+                        rowKey={(r, i) => {r._index = i; return i}} //生成行的key
+                        multiSelect={true}  //false 单选，默认多选
+                        scroll={{y: tableHeight}} //滚动轴高度
+                        height={28} //行高度
+                        bordered //表格有边界
+                        headerDisplayInRow={true}//表头换行用...来表示
+                        bodyDisplayInRow={true}//表体换行用...来表示
+                        headerHeight={40} //表头高度
+                        bodyStyle={{'height':tableHeight ,'background-color':'rgb(241, 242, 245)'}} //表体样式
+                        sheetHeader={{height: 30, ifshow: false}} //设置excel导出的表头的样式、支持height、ifshow
+                        hideHeaderScroll={false} //无数据时是否显示表头
+                        //排序属性设置
+                        sort={{
+                            mode: 'multiple', //多列排序
+                            backSource: false, //前端排序
+                        }}
+                        //分页对象
+                        paginationObj = {{
+                            activePage : this.props.queryParam.pageIndex,//活动页
+                            total : this.props.list.length,//总条数
+                            items: this.props.queryObj.totalPages,//总页数
+                            freshData: this.freshData, //活动页改变,跳转指定页数据
+                            dataNumSelect:['5','25','50','100'],
+                            dataNum:2,
+                            onDataNumSelect: this.onDataNumSelect, //每页行数改变,跳转首页
+                            verticalPosition:'bottom'
+                        }}
+                        rowClassName={(record,index,indent)=>{
+                            if (record._checked) {
+                                return 'selected';
+                            } else {
+                                return '';
+                            }
+                        }}
+                        onRowClick={this.onRowSelect}
+                        getSelectedDataFunc={this.getSelectedDataFunc}
+
+                    />
+            </div>
+        );
+    }
+}
+
+export default ListView;
