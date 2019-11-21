988c30122dc31af8d94e4897f6cfa0362ab09661
yjj <941296095@qq.com>
2019-11-21 13:53:29 +0800

    月末计提前台代码

--- ucf-apps/communication/src/routes/index.js
+++ ucf-apps/communication/src/routes/index.js
@@ -8,12 +8,14 @@ import { Route } from "mirrorx";
 import {CommunicationContract} from "./contract/container";
 import {CommunicationInvoice} from "./invoice/container";
 import {CommunicationCustomer} from "./customer/container"
+import {CommunicationAccrued} from "./accrued/container"
 
 export default () => (
     <div className="route-content">
         <Route exact path='/contract' component={CommunicationContract} />
         <Route exact path='/invoice' component={CommunicationInvoice} />
         <Route exact path='/customer' component={CommunicationCustomer} />
+        <Route exact path='/accrued' component={CommunicationAccrued} />
     </div>
 
 );
